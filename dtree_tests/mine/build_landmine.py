96f832457ac629e35a947486153ce3610c95fd17
kap00r <122755269+kap00r@users.noreply.github.com>
2024-10-06 15:46:00 -0400

    Update build_landmine.py

--- dtree_tests/mine/build_landmine.py
+++ dtree_tests/mine/build_landmine.py
@@ -11,7 +11,7 @@ from scapy.all import *
 seed = 99
 
 ### Import Data ###
-dataset = pd.read_excel("/home/admin1/netronome_tests/land_mine/land+mines-1/Mine_Dataset.xls",
+dataset = pd.read_excel("../dtree_tests/mine/land+mines-1/Mine_Dataset.xls",
                         sheet_name="Normalized_Data", names=['Voltage', 'Height', 'Soil Type', 'Mine Type'])
 
 dataset_features = dataset.drop(columns=['Mine Type'])
@@ -31,30 +31,18 @@ one_hot = OneHotEncoder(handle_unknown='ignore')
 X_train = np.hstack((np.delete(X_train, 2, 1), one_hot.fit_transform(X_train[:, 2].reshape(-1, 1)).toarray()))
 X_test = np.hstack((np.delete(X_test, 2, 1), one_hot.transform(X_test[:, 2].reshape(-1, 1)).toarray()))
 
-
 ### Data Translation for SmartNIC Models ###
 total_bits = 16
 dec_bits = 13
 
-def num_translate1(number, total_bits, dec_bits):
-    ret_val = 0
-    for i in range(total_bits-1, -1, -1):
-        comp_val = 2**(i - dec_bits)
-
-        if (number - comp_val >= 0):
-            ret_val += 2**i
-            number -= comp_val
-
-    return ret_val
-
-def num_translate2(number, total_bits, dec_bits):
+def num_translate(number, total_bits, dec_bits):
     return round(number * (2**dec_bits))
 
 hexTest = []
 testLen = len(X_test)
 for i in range(testLen):
-    intData = [num_translate2(feat, total_bits, dec_bits) for feat in X_test[i]]
-    
+    intData = [num_translate(feat, total_bits, dec_bits) for feat in X_test[i]]
+
     hexData = ""
     for feat in intData:
         hexFeat = "{:04x}".format(feat)
@@ -64,10 +52,8 @@ for i in range(testLen):
     hexTest.append(hexData)
 
 
-nicTest = PcapWriter("/home/admin1/netronome_tests/land_mine/nicTest.pcap")
-cpuTest = PcapWriter("/home/admin1/netronome_tests/land_mine/cpuTest.pcap")
-
-
+nicTest = PcapWriter("../dtree_tests/mine/nicTest.pcap")
+cpuTest = PcapWriter("../dtree_tests/mine/cpuTest.pcap")
 ### Add zero padding to make SmartNIC packets same size as CPU packets ###
 padding = b'\x00'
 for i in range(196):
@@ -75,20 +61,14 @@ for i in range(196):
 
 
 ### Test set too small, so repeat 1000 times for latency measure ###
-with open("/home/admin1/netronome_tests/land_mine/trueLabels.txt", "w") as test_file:  # ground truth labels for test set
+with open("../dtree_tests/mine/trueLabels.txt", "w") as test_file:  # ground truth labels for test set
     print(f"Test Len: {testLen}")
-    
-    ethHdr = Ether(dst="00:15:4d:13:79:ac", src="08:c0:eb:a6:de:3d", type=0x4d49)
+
+    ethHdr = Ether(dst="<dst_mac>", src="<src_mac>", type=0x4d49)
     ipHdr = IP(src="10.0.0.1", dst="10.0.0.2")
     udpHdr = UDP(dport=5005, sport=5005)
-    for i in range(testLen):
-        payload = Raw(load=hexTest[i] + padding)
-        pkt = ethHdr/ipHdr/udpHdr/payload
-        nicTest.write(pkt)
-        print("{}".format(y_test[i]), file=test_file)
 
-
-
+    # Latency test
     i = 0
     testIndex = 0
     while (i < 1000):
@@ -100,17 +80,21 @@ with open("/home/admin1/netronome_tests/land_mine/trueLabels.txt", "w") as test_
         i += 1
 
 
-### Build test set packets for CPU models ###
-ethHdr = Ether(dst="00:15:4d:13:79:ac", src="08:c0:eb:a6:de:3d", type=0x0800)
+    # Effectiveness test
+    for i in range(testLen):
+        payload = Raw(load=hexTest[i] + padding)
+        pkt = ethHdr/ipHdr/udpHdr/payload
+        nicTest.write(pkt)
+        print("{}".format(y_test[i]), file=test_file)
+
+
+
+### Test set packets for CPU models ###
+ethHdr = Ether(dst="<dst_mac>", src="<src_mac>", type=0x0800)
 ipHdr = IP(src="10.0.0.1", dst="10.0.0.2")
 udpHdr = UDP(dport=5005, sport=5005)
-for i in range(testLen):
-    payload = Raw(load=pickle.dumps(X_test[i].reshape(1, -1)))
-    pkt = ethHdr/ipHdr/udpHdr/payload
-    cpuTest.write(pkt)
 
-
-
+# Lateny test 
 i = 0
 testIndex = 0
 while (i < 1000):
@@ -121,3 +105,8 @@ while (i < 1000):
     testIndex = (testIndex + 1) % testLen
     i += 1
 
+# Effectiveness test
+for i in range(testLen):
+    payload = Raw(load=pickle.dumps(X_test[i].reshape(1, -1)))
+    pkt = ethHdr/ipHdr/udpHdr/payload
+    cpuTest.write(pkt)
