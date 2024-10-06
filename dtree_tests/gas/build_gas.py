02ad985650b5575a4ca3ff11443120429876fa95
kap00r <122755269+kap00r@users.noreply.github.com>
2024-10-06 16:16:23 -0400

    Update build_gas.py

--- dtree_tests/gas/build_gas.py
+++ dtree_tests/gas/build_gas.py
@@ -10,34 +10,34 @@ import pickle
 
 seed = 99
 
-batch1 = pd.read_csv("/home/admin1/netronome_tests/gas/gas+sensor+array+drift+dataset/batch1.dat", 
+batch1 = pd.read_csv("../dtree_tests/gas/gas+sensor+array+drift+dataset/batch1.dat",
                     header=None, sep=' \d+:')
 
-batch2 = pd.read_csv("/home/admin1/netronome_tests/gas/gas+sensor+array+drift+dataset/batch2.dat", 
+batch2 = pd.read_csv("../dtree_tests/gas/gas+sensor+array+drift+dataset/batch2.dat",
                     header=None, sep=' \d+:')
 
-batch3 = pd.read_csv("/home/admin1/netronome_tests/gas/gas+sensor+array+drift+dataset/batch3.dat", 
+batch3 = pd.read_csv("../dtree_tests/gas/gas+sensor+array+drift+dataset/batch3.dat",
                     header=None, sep=' \d+:')
 
-batch4 = pd.read_csv("/home/admin1/netronome_tests/gas/gas+sensor+array+drift+dataset/batch4.dat", 
+batch4 = pd.read_csv("../dtree_tests/gas/gas+sensor+array+drift+dataset/batch4.dat",
                     header=None, sep=' \d+:')
 
-batch5 = pd.read_csv("/home/admin1/netronome_tests/gas/gas+sensor+array+drift+dataset/batch5.dat", 
+batch5 = pd.read_csv("../dtree_tests/gas/gas+sensor+array+drift+dataset/batch5.dat",
                     header=None, sep=' \d+:')
 
-batch6 = pd.read_csv("/home/admin1/netronome_tests/gas/gas+sensor+array+drift+dataset/batch6.dat", 
+batch6 = pd.read_csv("../dtree_tests/gas/gas+sensor+array+drift+dataset/batch6.dat",
                     header=None, sep=' \d+:')
 
-batch7 = pd.read_csv("/home/admin1/netronome_tests/gas/gas+sensor+array+drift+dataset/batch7.dat", 
+batch7 = pd.read_csv("../dtree_tests/gas/gas+sensor+array+drift+dataset/batch7.dat",
                     header=None, sep=' \d+:')
 
-batch8 = pd.read_csv("/home/admin1/netronome_tests/gas/gas+sensor+array+drift+dataset/batch8.dat", 
+batch8 = pd.read_csv("../dtree_tests/gas/gas+sensor+array+drift+dataset/batch8.dat",
                     header=None, sep=' \d+:')
 
-batch9 = pd.read_csv("/home/admin1/netronome_tests/gas/gas+sensor+array+drift+dataset/batch9.dat", 
+batch9 = pd.read_csv("../dtree_tests/gas/gas+sensor+array+drift+dataset/batch9.dat",
                     header=None, sep=' \d+:')
 
-batch10 = pd.read_csv("/home/admin1/netronome_tests/gas/gas+sensor+array+drift+dataset/batch10.dat", 
+batch10 = pd.read_csv("../dtree_tests/gas/gas+sensor+array+drift+dataset/batch10.dat",
                     header=None, sep=' \d+:')
 
 
@@ -57,24 +57,14 @@ y_test = y_test.to_numpy()
 
 total_bits = 16
 dec_bits = 13
-def num_translate1(number, total_bits, dec_bits):
-    ret_val = 0
-    for i in range(total_bits-1, -1, -1):
-        comp_val = 2**(i - dec_bits)
 
-        if (number - comp_val >= 0):
-            ret_val += 2**i
-            number -= comp_val
-
-    return ret_val
-
-def num_translate2(number, total_bits, dec_bits):
+def num_translate(number, total_bits, dec_bits):
     return round(number * (2**dec_bits))
 
 hexTest = []
 testLen = len(X_test)
 for i in range(testLen):
-    intData = [num_translate2(feat, total_bits, dec_bits) for feat in X_test[i]]
+    intData = [num_translate(feat, total_bits, dec_bits) for feat in X_test[i]]
 
     hexData = ""
     for feat in intData:
@@ -85,8 +75,8 @@ for i in range(testLen):
     hexTest.append(hexData)
 
 
-nicTest = PcapWriter("/home/admin1/netronome_tests/gas/nicTest.pcap")
-cpuTest = PcapWriter("/home/admin1/netronome_tests/gas/cpuTest.pcap")
+nicTest = PcapWriter("../dtree_tests/gas/nicTest.pcap")
+cpuTest = PcapWriter("../dtree_tests/gas/cpuTest.pcap")
 
 
 ### Add zero padding to make SmartNIC packets same size as CPU packets ###
@@ -95,20 +85,14 @@ for i in range(919):
     padding= padding + b'\x00'
 
 
-with open("/home/admin1/netronome_tests/gas/trueLabels.txt", "w") as test_file:  # ground truth labels for test set
+with open("../dtree_tests/gas/trueLabels.txt", "w") as test_file:  # ground truth labels for test set
     print(f"Test Len: {testLen}")
 
-    ethHdr = Ether(dst="00:15:4d:13:79:ac", src="08:c0:eb:a6:de:3d", type=0x4d49)
+    ethHdr = Ether(dst="<dst_mac>", src="<src_mac>", type=0x4d49)
     ipHdr = IP(src="10.0.0.1", dst="10.0.0.2")
     udpHdr = UDP(dport=5005, sport=5005)
-    for testIndex in range(testLen):
-        payload = Raw(load=hexTest[testIndex] + padding)
 
-        pkt = ethHdr/ipHdr/udpHdr/payload
-        nicTest.write(pkt)
-        print("{}".format(y_test[testIndex]), file=test_file)
-
-
+    # Latency test
     i = 0
     testIndex = 0
     while (i < 1000):
@@ -120,16 +104,23 @@ with open("/home/admin1/netronome_tests/gas/trueLabels.txt", "w") as test_file: 
         i += 1
 
 
-### Best test set packets for CPU models ###
-ethHdr = Ether(dst="00:15:4d:13:79:ac", src="08:c0:eb:a6:de:3d", type=0x0800)
+    # Effectiveness test
+    for testIndex in range(testLen):
+        payload = Raw(load=hexTest[testIndex] + padding)
+
+        pkt = ethHdr/ipHdr/udpHdr/payload
+        nicTest.write(pkt)
+        print("{}".format(y_test[testIndex]), file=test_file)
+
+
+
+
+### Test set packets for CPU models ###
+ethHdr = Ether(dst="<dst_mac>", src="<src_mac>", type=0x0800)
 ipHdr = IP(src="10.0.0.1", dst="10.0.0.2")
 udpHdr = UDP(dport=5005, sport=5005)
-for i in range(testLen):
-    payload = Raw(load=pickle.dumps(X_test[i].reshape(1, -1)))
-    pkt = ethHdr/ipHdr/udpHdr/payload
-    cpuTest.write(pkt)
 
-
+# Latency test
 i = 0
 testIndex = 0
 while (i < 1000):
@@ -140,3 +131,9 @@ while (i < 1000):
     testIndex = (testIndex + 1) % testLen
     i += 1
 
+
+# Effectiveness test
+for i in range(testLen):
+    payload = Raw(load=pickle.dumps(X_test[i].reshape(1, -1)))
+    pkt = ethHdr/ipHdr/udpHdr/payload
+    cpuTest.write(pkt)
